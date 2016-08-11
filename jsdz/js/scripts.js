d4b86517ec2a286c932469fcecf57c63625b293a
4455jkjh <a4455jkjh@163.com>
2016-08-12 05:55:02 +0800

    jsdz

---
+++ jsdz/js/scripts.js
@@ -0,0 +1,36 @@
+var dz,wd,jz,bjz,wc,hint;
+dz = document.getElementById("dz");
+wd = document.getElementById("wd");
+jz = document.getElementById("jz");
+bjz = document.getElementById("bjz");
+wc = document.getElementById("wc");
+hint = document.getElementById("hint");
+
+function qkdz() {
+	//alert("清空电阻");
+	dz.value = "";
+}
+function qkwd() {
+	//alert("清空电阻");
+	wd.value = "";
+}
+function qkjz() {
+	//alert("清空电阻");
+	jz.value = "";
+}
+function qkAll() {
+	qkdz();
+	qkwd();
+	qkjz();
+}
+function input() {
+	//alert("input");
+	var dianzu=dz.value;
+	var wendu=wd.value;
+	var jizhun=jz.value;
+	var bijiaozhi=dianzu * 309.5 / (234.5 + wendu*1);
+	var wucha=(bijiaozhi*1 / jizhun*1 - 1) * 100;
+	bjz.value = "比较值：" + bijiaozhi;
+	wc.value = "误差：" + wucha + "%";
+	//hint.value = "dz:" + dianzu + "\nwd:" + wendu + "\njz:" + jizhun;
+}
