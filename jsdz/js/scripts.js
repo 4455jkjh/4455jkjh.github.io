c048f1f70e54bf7cc196d15a5ed23f1e7b78da85
4455jkjh <a4455jkjh@163.com>
2016-08-13 05:05:03 +0800

    fix bugs

--- jsdz/js/scripts.js
+++ jsdz/js/scripts.js
@@ -1,10 +1,9 @@
-var dz,wd,jz,bjz,wc,hint;
+var dz,wd,jz,bjz,wc;
 dz = document.getElementById("dz");
 wd = document.getElementById("wd");
 jz = document.getElementById("jz");
 bjz = document.getElementById("bjz");
 wc = document.getElementById("wc");
-hint = document.getElementById("hint");
 
 function qkdz() {
 	//alert("清空电阻");
@@ -28,9 +27,11 @@ function input() {
 	var dianzu=dz.value;
 	var wendu=wd.value;
 	var jizhun=jz.value;
+	if(!dianzu||!wendu||!jizhun){
+		return;
+	}
 	var bijiaozhi=dianzu * 309.5 / (234.5 + wendu*1);
 	var wucha=(bijiaozhi*1 / jizhun*1 - 1) * 100;
 	bjz.value = "比较值：" + bijiaozhi.toFixed(5);
 	wc.value = "误差：" + wucha.toFixed(5) + "%";
-	//hint.value = "dz:" + dianzu + "\nwd:" + wendu + "\njz:" + jizhun;
 }
